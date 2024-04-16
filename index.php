7f45dc16fa619b57f661854bba6ebd7ab6df12c8
DempseyPrimi <72927786+DempseyPrimi@users.noreply.github.com>
2024-04-15 22:45:57 -0600

    Fix Login

--- index.php
+++ index.php
@@ -21,8 +21,8 @@ if (isset($_SESSION['GYM']['nombre']) && $_SESSION['GYM']['nombre'] != "") {
       <body class="bg-gray-200" data-new-gr-c-s-check-loaded="14.1165.0" data-gr-ext-installed="">
             <!-- Creat By Joker Banny -->
             <div class="min-h-screen bg-gray-900 flex justify-center items-center">
-                  <div class="absolute w-60 h-60 rounded-xl bg-orange-300 -top-5 -left-16 z-0 transform rotate-45 hidden md:block"></div>
-                  <div class="absolute w-48 h-48 rounded-xl bg-orange-300 -bottom-6 -right-10 transform rotate-12 hidden md:block"></div>
+                  <div class="absolute w-60 h-60 rounded-xl bg-yellow-300 -top-5 -left-16 z-0 transform rotate-45 hidden md:block"></div>
+                  <div class="absolute w-48 h-48 rounded-xl bg-yellow-300 -bottom-6 -right-10 transform rotate-12 hidden md:block"></div>
                   <form id="form-log" class="py-12 px-12 bg-white rounded-2xl shadow-xl z-20">
                         <div>
                               <h1 class="text-3xl font-bold text-center mb-4 cursor-pointer">Iniciar Sesión</h1>
@@ -35,11 +35,11 @@ if (isset($_SESSION['GYM']['nombre']) && $_SESSION['GYM']['nombre'] != "") {
                               <input class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required="" autocomplete="current-password" type="password" name="passwd" placeholder="Contraseña">
                         </div>
                         <div class="text-center mt-6">
-                              <button type="submit" class="w-full py-2 text-xl text-white bg-orange-300 rounded-lg hover:bg-yellow-500 transition-all">Ingresar</button>
+                              <button type="submit" class="w-full py-2 text-xl text-white bg-yellow-500 rounded-lg hover:bg-yellow-500 transition-all">Ingresar</button>
                         </div>
                   </form>
-                  <div class="w-40 h-40 absolute bg-orange-100 rounded-full top-0 right-12 hidden md:block"></div>
-                  <div class="w-20 h-40 absolute bg-orange-200 rounded-full bottom-20 left-10 transform rotate-45 hidden md:block"></div>
+                  <div class="w-40 h-40 absolute bg-yellow-100 rounded-full top-0 right-12 hidden md:block"></div>
+                  <div class="w-20 h-40 absolute bg-yellow-200 rounded-full bottom-20 left-10 transform rotate-45 hidden md:block"></div>
             </div>
 
 
